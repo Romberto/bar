01e38b5d5aac7f429d8fb6dc53fc165c4fc0fcdf
unknown <korhoff@mail.ru>
2022-07-31 13:24:54 +0400

    menu

--- js/main.js
+++ js/main.js
@@ -1,27 +1,20 @@
 window.addEventListener('load', function(){
     $('.header__menu_link').on('click', function(e){
-    $('.header__popup').toggleClass('popup_is_active')
+    $('.header__menu').fadeIn()
     $('#line2').toggleClass('close__popup_none')
     $('#line1').toggleClass('close__popup_rotete')
     $('#line3').toggleClass('close__popup_rotete_45')
     });
 
 
-    $('.piramide__foto').slick({
-        arrows: true,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
-        prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                arrows: false
-              }
-            }
-        ]
+    $('.menu__burger').click(function(e){
+      $('.header__menu').fadeOut()
+    })
 
-    });
+    $(document).mouseup(function (e) {
+      var container = $(".header__menu");
+      if (container.has(e.target).length === 0){
+          container.fadeOut();
+      }
+  });
 });
