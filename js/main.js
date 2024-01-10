03e0620f136da9d8e7512b4b1ab9b84738948fdc
romberto <korhoff@mail.ru>
2024-01-10 22:08:21 +0400

    замена фото на странице бармен на выезд

--- js/main.js
+++ js/main.js
@@ -34,6 +34,36 @@ window.addEventListener('load', function(){
     }
   });
 
+  const swiper = new Swiper('.swiper', {
+    effect: "cube",
+      grabCursor: true,
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
+    // Optional parameters
+    
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
 
   
 });
